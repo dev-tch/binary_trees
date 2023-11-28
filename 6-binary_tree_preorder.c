6daf5dcfbe9fb5a61e85d9fbf93a0e1ded860271
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-28 21:53:04 +0000

    traversal preorder

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ * @tree: pointer to tree
+ * @func: function pointer
+ * Return: void
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+}

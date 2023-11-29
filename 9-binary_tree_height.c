8f2042898a5eda82325303bb5a2f1503d96f297a
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-29 00:38:12 +0000

    fix height tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-#include <math.h>
+size_t _max(size_t a, size_t b);
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node
@@ -15,5 +15,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	len_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	len_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (fmax(len_left, len_right));
+	return (_max(len_left, len_right));
+}
+
+size_t _max(size_t a, size_t b)
+{
+	return (a > b ? a : b);
 }

e3fd536596d40025d17d9a2886900feadd9db317
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-29 19:32:06 +0000

    fix count

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,11 +7,16 @@
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t cpt = 0;
+
 	if (!tree)
 		return (0);
 	if (tree->left || tree->right)
-		return (1);
-	else
-		return (0);
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	{
+		cpt = 1;
+	}
+	cpt += binary_tree_nodes(tree->left);
+	cpt += binary_tree_nodes(tree->right);
+
+	return (cpt);
 }

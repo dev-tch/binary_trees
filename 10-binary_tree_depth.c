3fe7a0927ab754b016c82321128a0e1821f5c95a
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-29 01:11:43 +0000

    fix task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -12,6 +12,6 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	}
 	else
 	{
-		return (1 + binary_tree_depth(tree->parent));
+		return (tree->parent ? 1 + binary_tree_depth(tree->parent) : 0);
 	}
 }
